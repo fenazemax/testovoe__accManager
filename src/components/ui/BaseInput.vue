5e00d32e1425a42560b78d1dd4bf77bdf4ffcbe2
fenazemax <maximaxaspotlight@gmail.com>
2025-08-06 15:20:45 +0500

    add defineOptions for inheritAttrs:false

--- src/components/ui/BaseInput.vue
+++ src/components/ui/BaseInput.vue
@@ -3,6 +3,9 @@ import { NInput, NFormItem } from 'naive-ui'
 import { useField } from 'vee-validate'
 import { computed } from 'vue'
 
+defineOptions({
+  inheritAttrs: false,
+})
 const model = defineModel<string>()
 const props = defineProps<{
   required?: boolean
